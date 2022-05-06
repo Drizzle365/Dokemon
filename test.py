bb86f10a99fc63b45c638181f5763ea6b0c5fbe2
Drizzle <i@drizzle.vip>
2022-05-06 19:22:04 +0800

    Add 890 Pokemon vertical drawing resources

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-from data.user import User
+import requests
 
-user = User()
-print(user.db)
+for i in range(4, 891):
+    res = requests.get('https://s.pokeuniv.com/pokemon/sprite/front/%s.gif' % i).content
+    f = open('temp/%s.gif' % i, 'wb')
+    f.write(res)
+    print('第 %s 张图片爬取成功' % i)
