00663302ab286537ab2a12bd482e4656839a9a3c
Drizzle <i@drizzle.vip>
2022-05-09 08:57:09 +0800

    init game.js

--- view/src/page/game.js
+++ view/src/page/game.js
@@ -1,17 +1,27 @@
 import React from "react";
-import {Spin} from 'antd';
+import {Button} from 'antd';
 
 export default class Game extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
+        this.state = {};
     }
 
     render() {
-        return <h1 style={{textAlign: 'center'}}><Spin/> 开发中</h1>
+        return (
+            <div style={{textAlign: 'center'}}>
+                <div>
+                    <Button>地图</Button>
+                    <Button>设施</Button>
+                    <Button>任务</Button>
+                    <Button>交易</Button>
+                    <Button>签到</Button>
+                    <Button>礼包</Button>
+                    <Button>活动</Button>
+                    <Button>排行</Button>
+                </div>
+            </div>
+        )
     }
 
 }
