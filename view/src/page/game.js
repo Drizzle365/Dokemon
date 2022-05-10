7d63a2858e4347f9e5d8f0034ed7d635052ccefa
Drizzle <i@drizzle.vip>
2022-05-10 11:30:58 +0800

    ico

--- view/src/page/game.js
+++ view/src/page/game.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Button} from 'antd';
+import {Button, Avatar, Tag} from 'antd';
 import './game.css'
+import {service} from "../config";
 
 export default class Game extends React.Component {
     constructor(props) {
@@ -11,15 +12,24 @@ export default class Game extends React.Component {
     render() {
         return (
             <div className={'game'}>
-                <div className={'top-nav'}>
-                    <Button>地图</Button>
-                    <Button>设施</Button>
-                    <Button>任务</Button>
-                    <Button>交易</Button>
-                    <Button>签到</Button>
-                    <Button>礼包</Button>
-                    <Button>活动</Button>
-                    <Button>排行</Button>
+                <div className={'main'}>
+                    <div className={'info'}>
+                        <div><Avatar src={service + 'static/img/logo.png'} size={"large"}
+                                     style={{margin: '-20px 0 5px 0'}}></Avatar></div>
+                        <div style={{marginBottom: '5px'}}>
+                            <Tag style={{marginLeft: '12px'}} color="#f50">称号：初级训练师</Tag>
+                        </div>
+                    </div>
+                    <div className={'top-nav'}>
+                        <Button>地图</Button>
+                        <Button>设施</Button>
+                        <Button>任务</Button>
+                        <Button>交易</Button>
+                        <Button>签到</Button>
+                        <Button>礼包</Button>
+                        <Button>活动</Button>
+                        <Button>排行</Button>
+                    </div>
                 </div>
             </div>
         )
