04c8e20477fbea5a99b7c800388bee83e25a5978
Drizzle <i@drizzle.vip>
2022-05-14 16:38:21 +0800

    Main interface reconstruction

--- routers/json.py
+++ routers/json.py
@@ -11,8 +11,12 @@ js_dokemon = JsonSelect('dokemon')
 def get_map(mid: int):
     m = js_map.get_map(mid)
     m['npc_list'] = []
+    m['dokemon_list'] = []
     for item in str(m['npc']).split():
         m['npc_list'].append({'id': item, 'name': js_npc.get_npc(int(item))['name']})
+    d = str(m['dokemon']).split()
+    for i in range(0, len(d), 2):
+        m['dokemon_list'].append({'id': d[i], 'name': js_dokemon.get_dokemon(int(d[i]))['name'], 'lv': d[i + 1]})
     return m
 
 
--- view/src/page/map.js
+++ view/src/page/map.js
@@ -8,19 +8,10 @@ import {useNavigate} from "react-router-dom";
 export default () => {
     const navigate = useNavigate()
     const [role, setRole] = useState({
-        coin: '',
-        designation: '',
-        diamond: '',
-        map: '',
-        name: '',
-        sex: '',
-        uid: ''
+        coin: '', designation: '', diamond: '', map: '', name: '', sex: '', uid: ''
     });
     const [mmap, setMap] = useState({
-        name: '',
-        depiction: '',
-        npc: '',
-        npc_list: false
+        name: '', depiction: '', npc: '', npc_list: false, dokemon_list: false
     })
     useEffect(() => {
         axios.get(SERVICE + 'role?token=' + cookie.load('token')).then(r => {
@@ -30,17 +21,20 @@ export default () => {
             })
         })
     }, [])
-    return (
+    return (<div>
+        <h1 className={'main_top'}>{mmap.name}</h1>
+        <p>{mmap.depiction}</p>
+        <h2>这里有:</h2>
+        <div>{mmap.npc_list ? mmap.npc_list.map((item, index) => (<span key={index} onClick={() => {
+            navigate('/game/npc/' + item.id)
+        }} className={'link'}><img style={{height: '25px', margin: '2px'}} alt={'!'}
+                                   src={SERVICE_IMG + 'ui/!.png'}/>{item.name}</span>)) : '加载中'}</div>
         <div>
-            <h1 className={'main_top'}>{mmap.name}</h1>
-            <p>{mmap.depiction}</p>
-            <h2>这里有:</h2>
-            {mmap.npc_list ? mmap.npc_list.map((item, index) => (
-                <span key={index} onClick={() => {
-                    navigate('/game/npc/' + item.id)
-                }} className={'link'}><img style={{height: '25px', margin: '2px'}} alt={'!'}
-                                           src={SERVICE_IMG + 'ui/!.png'}/>{item.name}</span>
-            )) : '加载中'}
+            {mmap.dokemon_list ? mmap.dokemon_list.map((item, index) => (<span key={index} onClick={() => {
+                console.log(item)
+            }} className={'link'} style={{color: '#32327a'}}><img style={{height: '25px', margin: '2px'}}
+                                                                  alt={'!'}
+                                                                  src={SERVICE_IMG + 'pokemon/' + item.id + '.gif'}/>{item.name}</span>)) : '加载中'}
         </div>
-    )
+    </div>)
 }
