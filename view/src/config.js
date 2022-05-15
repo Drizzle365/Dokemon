d3975b07f540b005d799d8988fcdd145ff67da65
Drizzle <i@drizzle.vip>
2022-05-15 10:37:47 +0800

    readme

--- view/src/config.js
+++ view/src/config.js
@@ -1,3 +1,8 @@
-const SERVICE = 'http://localhost:8000/';
-const SERVICE_IMG = 'http://localhost:8000/static/img/';
+const flag = false;
+let SERVICE = 'http://localhost:8000/';
+let SERVICE_IMG = 'http://localhost:8000/static/img/';
+if (flag) {
+    SERVICE = 'http://43.156.43.203:8000/';
+    SERVICE_IMG = 'http://43.156.43.203:8000/static/img/';
+}
 export {SERVICE, SERVICE_IMG}
