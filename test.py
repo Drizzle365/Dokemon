75ca1f18480a32a22a43839a62cbf784ec364984
Drizzle <i@drizzle.vip>
2022-05-17 22:46:06 +0800

    static

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-import datetime
+import asyncio
 
-print()
+import requests
+
+
+async def down(url):
+    loop_down = asyncio.get_event_loop()
+    future = loop_down.run_in_executor(None, requests.get, 'https://s.pokeuniv.com/pokemon/sprite/back/%s.gif' % url)
+    res = await future
+    print(url)
+    f = open('temp/%s.gif' % url, 'wb')
+    f.write(res.content)
+
+
+if __name__ == '__main__':
+    task = [down(url) for url in range(1, 802)]
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(task))
