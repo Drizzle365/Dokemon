5fb062be303fc5b527f2531151a5698af1accd85
Drizzle <i@drizzle.vip>
2022-05-20 22:11:03 +0800

    readme

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-import json
-import requests
-
-f = open('model/json/dokemon.json', 'r', encoding='utf-8')
-res: dict = json.load(f)
-for i in res.keys():
-    if '.' in i:
-        img = requests.get('https://s.pokeuniv.com/pokemon/sprite/front/%s.gif' % i)
-        with open('static/img/pokemon/front/%s.gif' % i, 'wb') as f_img:
-            f_img.write(img.content)
-        img = requests.get('https://s.pokeuniv.com/pokemon/sprite/back/%s.gif' % i)
-        with open('static/img/pokemon/back/%s.gif' % i, 'wb') as f_img:
-            f_img.write(img.content)
-            print(i)
+import openpyxl
